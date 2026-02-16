fcc7fb45744a6e1329dd6e8ff6667846f7b81284
Rizkyaditiya21 <rizkyaditiyafernando@gmail.com>
2026-02-16 13:57:52 +0700

    Create config.js

---
+++ data/config.js
@@ -0,0 +1,27 @@
+{
+ "pg":[
+  {
+   "name":"Mahjong Ways 2",
+   "folder":"mahjong",
+   "image":"assets/images/pg-mahjong.jpg"
+  },
+  {
+   "name":"Lucky Neko",
+   "folder":"neko",
+   "image":"assets/images/pg-neko.jpg"
+  }
+ ],
+
+ "pragmatic":[
+  {
+   "name":"Sweet Bonanza",
+   "folder":"sweet",
+   "image":"assets/images/pra-sweet.jpg"
+  },
+  {
+   "name":"Sugar Rush",
+   "folder":"sugar",
+   "image":"assets/images/pra-sugar.jpg"
+  }
+ ]
+}
